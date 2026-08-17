7a37522a88262fb627e950d38213ce607b19bd27
Lucien Rochery <lucien.rochery@hotmail.com>
2026-08-17 14:57:53 +0200

    Formatting and logic fix.

--- src/Adaptation/Insertion/EdgeSeed.cxx
+++ src/Adaptation/Insertion/EdgeSeed.cxx
@@ -14,101 +14,91 @@
 #include "../../low_topo.hxx"
 #include "../../utils/mprintf.hxx"
 
-namespace Metris {
+namespace Metris{
 
 class MshCavity;
 
 EdgeSeed::EdgeSeed(MeshBase& msh, MshCavity& cav_, int tdim_adp_, int tdim_ent, int ientt, int iedl, bool icollapse, int ithrd) : tdim_adp(tdim_adp_), cav(cav_) {
-    
-    ierro = 0;
-    obj = NULL;
-    tdimp = -1;
-    iseed = -1;
-    iref = -1;
+  
+  ierro = 0;
+  obj = NULL;
+  tdimp = -1;
+  iseed = -1;
+  iref  = -1;
 
-    cav.reset();
-    cav.lcedg.allocate(1);
-    cav.lcfac.allocate(10);
-    if (msh.get_tdim() >= 3)
-        cav.lctet.allocate(10);
+  cav.reset();
+  cav.lcedg.allocate(1);
+  cav.lcfac.allocate(10);
+  if(msh.get_tdim() >= 3) cav.lctet.allocate(10);
 
-    const auto lnoed = tdim_ent == 1 ? lnoed1 : tdim_ent == 2 ? lnoed2 : lnoed3;
+  const auto lnoed = tdim_ent == 1 ? lnoed1 : 
+                      tdim_ent == 2 ? lnoed2 : lnoed3;
 
-    ipedg[0] = msh.ent2poi(tdim_ent)(ientt, lnoed[iedl][0]);
-    ipedg[1] = msh.ent2poi(tdim_ent)(ientt, lnoed[iedl][1]);
+  ipedg[0] = msh.ent2poi(tdim_ent)(ientt,lnoed[iedl][0]);
+  ipedg[1] = msh.ent2poi(tdim_ent)(ientt,lnoed[iedl][1]);
 
-    int iopen;
-    shell(msh, ipedg[0], ipedg[1], tdim_ent, ientt, cav.lcedg, cav.lcfac, cav.lctet, &iopen);
+  int iopen;
+  shell(msh,ipedg[0],ipedg[1],tdim_ent,ientt,cav.lcedg,cav.lcfac,cav.lctet,&iopen);
 
-    // Dimension of the entity holding the edge, read before the collapse balls
-    // widen the cavity below.
-    int shell_dim = -1;
-    if (cav.lcedg.get_n() > 0)
-        shell_dim = 1;
-    else if (cav.lcfac.get_n() > 0)
-        shell_dim = 2;
-    else
-        shell_dim = 3;
+  // Dimension of the edge proper, as gathered from the shell. Necessary to ensure collapse compatibility.
+  int shell_dim = -1;
+  if (cav.lcedg.get_n() > 0)
+      shell_dim = 1;
+  else if (cav.lcfac.get_n() > 0)
+      shell_dim = 2;
+  else
+      shell_dim = 3;
 
-    if (icollapse) {
-        int pdim0 = msh.getpoitdim(ipedg[0]);
-        int pdim1 = msh.getpoitdim(ipedg[1]);
-        // Collapse compatibility. Two ends both constrained to an entity the edge does
-        // not lie in are not adjacent there, so merging them tears it. One end alone
-        // being lesser dim is fine: the other is simply removed onto it.
-        if (shell_dim > MAX(pdim0, pdim1)) {
-            ierro = INS2D_ERR_COLEDGDIM;
-            return;
-        }
-        // A collapse removes both ends, so their balls are cavity seeding, not growth.
-        for (int ii = 0; ii < 2; ii++)
-            ball(msh, ipedg[ii], cav.lcedg, cav.lcfac, cav.lctet, &iopen, true, ithrd);
-    }
+  if (icollapse) {
+      int pdim0 = msh.getpoitdim(ipedg[0]);
+      int pdim1 = msh.getpoitdim(ipedg[1]);
+      // Collapse compatibility. If any of the boundary points is lower dimension than the edge,
+      // then the points are either incompatible OR (if only one is boundary), we need to do this 
+      // as a VERTEX collapse of the higher dimensional point.
+      if (shell_dim > MIN(pdim0, pdim1)) {
+          ierro = INS2D_ERR_COLEDGDIM;
+          return;
+      }
+      // Seed collapse cavity.
+      for (int ii = 0; ii < 2; ii++)
+          ball(msh, ipedg[ii], cav.lcedg, cav.lcfac, cav.lctet, &iopen, true, ithrd);
+  }
 
-    // Read from the seeded cavity. A ball reaches no lower than its point's own
-    // dimension, so for a collapse this is MIN(pdim0, pdim1): the dimension the
-    // merged point inherits, whether or not the ends share one.
-    tdimp = -1;
-    if (cav.lcedg.get_n() > 0)
-        tdimp = 1;
-    else if (cav.lcfac.get_n() > 0)
-        tdimp = 2;
-    else
-        tdimp = 3;
+  tdimp = -1;
+       if(cav.lcedg.get_n() > 0) tdimp = 1;
+  else if(cav.lcfac.get_n() > 0) tdimp = 2;
+  else                           tdimp = 3;
 
-    if (tdimp == 1) {
-        int iedge = cav.lcedg[0];
-        METRIS_ASSERT(iedge >= 0);
-        iseed = iedge;
-        iref = msh.edg2ref[iedge];
-        if (msh.isboundary_edges() && msh.CAD())
-            obj = msh.CAD.cad2edg[iref];
-    } else if (tdimp == 2) {
-        int iface = cav.lcfac[0];
-        METRIS_ASSERT(iface >= 0);
-        iseed = iface;
-        iref = msh.fac2ref[iface];
-        if (msh.isboundary_faces() && msh.CAD())
-            obj = msh.CAD.cad2fac[iref];
-    } else {
-        iseed = ientt;
-        iref = msh.tet2ref[ientt];
-    }
+  if(tdimp == 1){
+      int iedge = cav.lcedg[0];
+      METRIS_ASSERT(iedge >= 0);
+      iseed = iedge;
+      iref = msh.edg2ref[iedge];
+      if(msh.isboundary_edges() && msh.CAD())
+          obj = msh.CAD.cad2edg[iref];
+  }else if(tdimp == 2){
+      int iface = cav.lcfac[0];
+      METRIS_ASSERT(iface >= 0);
+      iseed = iface;
+      iref  = msh.fac2ref[iface];
+      if(msh.isboundary_faces() && msh.CAD())
+          obj = msh.CAD.cad2fac[iref];
+  }else{
+      iseed = ientt;
+      iref = msh.tet2ref[ientt];
+  }
 
 #ifndef NDEBUG
-    bool ithrow = !(iseed >= 0 && iseed < msh.nentt(tdimp)) ||
-                  isdeadent(iseed, msh.ent2poi(tdimp)) || iref < 0;
-    if (ithrow)
-        METRIS_THROW_MSG(
-                "## EdgeSeed failed: tdimp = {} iseed = {} iref = {}\n"
-                "Called with tdim = {} ientt = {} iedl = {}\n",
-                tdimp,
-                iseed,
-                iref,
-                tdim_ent,
-                ientt,
-                iedl);
+  bool ithrow = !(iseed >= 0 && iseed < msh.nentt(tdimp))
+             ||  isdeadent(iseed,msh.ent2poi(tdimp))
+             ||  iref < 0;
+  if(ithrow) METRIS_THROW_MSG(
+              "## EdgeSeed failed: tdimp = {} iseed = {} iref = {}\n"
+              "Called with tdim = {} ientt = {} iedl = {}\n",
+              tdimp,iseed,iref,tdim_ent,ientt,iedl);
 #endif
 }
 
-}  // namespace Metris
+
+
+}// namespace Metris
